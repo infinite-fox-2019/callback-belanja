492a823e5c094681ffaecbea02b4685dbe19778b
Ricky <audrickong.ao@gmail.com>
2019-09-12 17:54:37 +0700

    Revision

--- beli.js
+++ beli.js
@@ -2,14 +2,12 @@ function beli(uang, obj, cb){
   console.log(`Saya pergi membeli ${obj.item}`)
   setTimeout(function(){
     let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+    if (kembalian >= 0) {
       console.log(`Saya sudah membeli ${obj.item}, uang kembaliannya ${kembalian}`);
-      console.log('==================================================')
       cb(kembalian)
     }else{
-      console.log(`Uang gk cukup untuk beli ${obj.item} kembaliannya cuma ${kembalian}, coba bawa banyakan besok-besok`);
-      console.log('==================================================')
-      cb(0)
+      console.log(`Uang gk cukup untuk beli ${obj.item} uangnya cuma ${uang}`);
+      cb(uang)
     }
   }, obj.waktu);
 }
