25ff642ba9d26b6d605f549bd2f996cf92a5adc3
adityain98 <adityain98@gmail.com>
2019-09-12 14:42:34 +0700

    perbaikan

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ let sereh =  {item: 'sereh', harga: 2000, waktu: 500};
 let bawang =  {item: 'bawang', harga: 3000, waktu: 600};
 let ayam = {item: 'ayam', harga: 30000, waktu: 1000}
 
-beli(60000,cabe, function(kembalian){
+beli(10000,cabe, function(kembalian){
     beli(kembalian, ikan, function(kembalian2){
         beli(kembalian2, sereh, function(kembalian3){
             beli(kembalian3, bawang, function(kembalian4){
