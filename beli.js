4fa19037ce00b5abf7f1028ea6a1c841c1265652
afifahrahmak <afifahrk96@gmail.com>
2019-09-12 15:54:38 +0700

    callback-belanja

--- beli.js
+++ beli.js
@@ -1,13 +1,12 @@
 function beli(uang, obj, cb){
-  console.log(`Saya pergi membeli ${obj.item}`)
   setTimeout(function(){
     let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+    if (kembalian > obj.harga) {
+      console.log(`Saya pergi membeli ${obj.item}`)
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
-    }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+    }else if (kembalian < obj.harga){
+      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian+obj.harga}`);
     }
   }, obj.waktu);
 }
---
+++ index.js
@@ -0,0 +1,44 @@
+const beli = require('./beli')
+
+const sayur = {
+    item: 'sayur',
+    harga: 3000,
+    waktu: 200
+}
+const buah = {
+    item: 'buah',
+    harga: 10000,
+    waktu: 200
+}
+const beras = {
+    item: 'beras',
+    harga: 15000,
+    waktu: 200
+}
+const daging = {
+    item: 'daging',
+    harga: 3000,
+    waktu: 200
+}
+const udang = {
+    item: 'udang',
+    harga: 20000,
+    waktu: 200
+}
+
+let uang = 50000
+
+const belanja = beli(uang,sayur,function(kembalian){
+    beli(kembalian, buah, function(kembalian){
+        beli(kembalian, beras, function(kembalian){
+            beli(kembalian, daging, function(kembalian){
+                beli(kembalian, udang,function(kembalian){
+                    return ''
+                })
+            })
+        })
+    })
+})
+
+
+console.log(belanja)
