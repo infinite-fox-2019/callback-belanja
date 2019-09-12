b6510260e0124c32a6ddb1f762da7dc99f3dde6f
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-09-12 15:16:04 +0700

    callback belanja

--- beli.js
+++ beli.js
@@ -2,12 +2,12 @@ function beli(uang, obj, cb){
   console.log(`Saya pergi membeli ${obj.item}`)
   setTimeout(function(){
     let kembalian = uang - obj.harga
-    if (kembalian > 0) {
+    if (kembalian >= 0) {
       console.log(`Saya sudah membeli ${obj.item} uang kembaliannya ${kembalian}`);
       cb(kembalian)
     }else{
-      console.log(`uang gk cukup nih buat beli ${obj.item} kembaliannya cuma ${kembalian}`);
-      cb(0)
+      console.log(`uang gak cukup nih buat beli ${obj.item} uangnya cuma punya ${uang} padahal harga ${obj.item} adalah ${obj.harga} `);
+      cb(uang)
     }
   }, obj.waktu);
 }
